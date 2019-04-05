7d11344903aebcd63afce19340834de0e26eae50
ldonjibson <ajibolaolayanju@gmail.com>
2019-04-05 22:54:58 +0100

    Middleware used to set permission to access some urls

---
+++ server/middleware/verifyuserlogin.js
@@ -0,0 +1,54 @@
+const express = require('express')
+let bodyParser = require('body-parser');
+let bcrypt = require('bcrypt-nodejs'); // used to encrypt password
+let jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
+
+let helper = require('../helpers/helper')
+let users = require('../datastore/user.js')
+
+let server = express();
+const router = express.Router();
+let url = '/api/v1/';
+
+let config = require('../config/config.js')
+
+server.set('superSecret', config.secret);
+
+// router.use(bodyParser.urlencoded({ extended: false }));
+// router.use(bodyParser.json({ type: 'application/json'}));
+
+//route midleware to verify atoken
+
+const jwtVerify= ((req, res, next) => {
+	// check header or url parameters or post parameteers for token
+	try {
+	let token = req.body.token || req.query.token || req.headers['x-access-token'];
+	//decode token
+	if(token) {
+		//verifies secret and check up
+		jwt.verify(token, server.get('superSecret'), (err, decoded) => {
+			if(err) {
+				return res.json({status:1002, error: 'Failed to Authenticate token'});
+			} else {
+				//if authenticatable save to request for other route to use
+				req.decoded = decoded;
+				// console.log(req.decoded);
+				next();
+				// res.status(200).send();
+			}
+		})
+	} else {
+		//if there is no token return an error
+		return res.json({
+			status: 1004,
+			error: "No token provided."
+		});
+	}
+	}
+	catch (err){
+		return console.log(err);
+		}
+});
+
+module.exports = jwtVerify;
+// server.use('', router);
