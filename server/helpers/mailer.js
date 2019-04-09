20e3a32e9cde75a7e55605493c75d5d3035379d0
ldonjibson <ajibolaolayanju@gmail.com>
2019-04-09 16:25:57 +0100

    mailer script added to send email notification on debit/credit and passsword change

---
+++ server/helpers/mailer.js
@@ -0,0 +1,16 @@
+let nodeMailer = require(nodemailer);
+
+let transporter = nodeMailer.createTransport({
+	host: 'smtp.gmail.com',
+	port: 465,
+	secure: true,
+	auth: {
+	    user: 'ckagoxozic@gmail.com',
+	    pass: 'nollywood10'
+	}
+});
+
+//exports
+module.exports = {
+	transporter,
+}
