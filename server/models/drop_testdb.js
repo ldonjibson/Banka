b90fc15b16c88ba114bdfcd7cbdb7b6c9fad3bbd
ldonjibson <ajibolaolayanju@gmail.com>
2019-04-25 15:36:05 +0100

    chore(create-database-and-models) [DELIVER #165485561]

--- server/models/drop_testdb.js
+++ server/models/drop_testdb.js
@@ -1,12 +1,12 @@
+import {pool} from '../db/index'
+let clienty = pool
+import format from 'pg-format';
+
 const dropTables = () =>{
-	return new Promise((resolve, reject) =>{
-			clienty.query(`DROP TABLE IF EXISTS  users, bankaccount, transaction CASCADE`, (err, res) => {
-					if (err){
-						console.log(err);
-					}else {
-						console.log('tables dropped');
-				}
-			});
-		}	
-	)
+	clienty.query(`DROP TABLE IF EXISTS  users, bankaccount, transaction CASCADE`)
+	.then(res => {
+		console.log('tables dropped');
+	}).catch(error=> { console.log(error)})
 }
+
+dropTables(); 
