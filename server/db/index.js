15ee25fb43570d8304b774c246afa1882ef12157
ldonjibson <ajibolaolayanju@gmail.com>
2019-04-27 12:45:11 +0100

    chore for continuous integration

--- server/db/index.js
+++ server/db/index.js
@@ -3,13 +3,15 @@ import { Pool, Client } from 'pg';
 
 dotenv.config();
 
+const connectionString = `postgres://postgres@127.0.0.1:5432/DB_NAME${process.env.DB_NAME}`
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
+  // user: process.env.DB_USER,
+  // host: process.env.DB_HOST,
+  // database: process.env.DB_NAME,
+  // password: process.env.DB_PASS,
+  // port: process.env.DB_PORT,
+	connectionString: connectionString,
 });
 
 
