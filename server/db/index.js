d2da47219998b967ac741c19cf069f676b587d05
ldonjibson <ajibolaolayanju@gmail.com>
2019-04-28 22:40:19 +0100

    deploy to heroku

--- server/db/index.js
+++ server/db/index.js
@@ -6,12 +6,12 @@ dotenv.config();
 const connectionString = `postgres://jfnwebwmbjzxme:c0d9578448b40a3a9bd186963c6b206e953305008b46b0ebb9785d743ff0665a@ec2-23-23-241-119.compute-1.amazonaws.com:5432/d1mu1qpl67gnqg`
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT
-	// connectionString: connectionString,
+  // user: process.env.DB_USER,
+  // host: process.env.DB_HOST,
+  // database: process.env.DB_NAME,
+  // password: process.env.DB_PASS,
+  // port: process.env.DB_PORT
+	connectionString: connectionString,
 });
 
 
